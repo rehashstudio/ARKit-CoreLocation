b3a8844220ac6c5f622589f828756ad217b53433
Stephen Schieberl <bantherewind@gmail.com>
2020-11-13 13:15:54 -0800

    Version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ARCL",
-    platforms: [ .iOS(.v13.4) ],
+    platforms: [ .iOS(.v14) ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "ARCL", targets: ["ARKit-CoreLocation"])
